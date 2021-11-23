76b491f70f8ab199aaa4412a2964e0e23c178d2d
Alejandro Lima <73153439+Drojann@users.noreply.github.com>
2021-11-22 20:20:05 -0600

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { InventarioComponent } from './inventario/inventario.component';
+import { LoginComponent } from './login/login.component';
+import { NavegacionComponent } from './navegacion/navegacion.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //{ path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'login', component: LoginComponent},
+  { path: 'home', component: DashboardComponent},
+  { path: 'navegacion', component: NavegacionComponent},
+  { path: 'inventario', component: InventarioComponent},
+  //{ path: '**', pathMatch: 'full', redirectTo: 'login' },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,42 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+//componentes
+import { AppComponent } from './app.component';
+import { NavegacionComponent } from './navegacion/navegacion.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { ChatbotComponent } from './chatbot/chatbot.component';
+import { InventarioComponent } from './inventario/inventario.component';
+
 //angular materials
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavegacionComponent } from './navegacion/navegacion.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatMenuModule } from '@angular/material/menu';
-import { LoginComponent } from './login/login.component';
 import { MatFormFieldModule} from '@angular/material/form-field';
 import { MatInputModule} from '@angular/material/input';
-import { ChatbotComponent } from './chatbot/chatbot.component';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { environment } from '../environments/environment';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+
+//firebase
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+
 
 @NgModule({
   declarations: [
@@ -31,7 +44,8 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
     NavegacionComponent,
     DashboardComponent,
     LoginComponent,
-    ChatbotComponent
+    ChatbotComponent,
+    InventarioComponent
 
   ],
   imports: [
@@ -51,7 +65,15 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
     MatInputModule,
     MatProgressSpinnerModule,
     ReactiveFormsModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+
+
   ],
   providers: [],
   bootstrap: [AppComponent],
