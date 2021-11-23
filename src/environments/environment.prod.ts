cdba58d0aaeddefb1eb28f56995a24b725b378df
Alejandro Lima <73153439+Drojann@users.noreply.github.com>
2021-11-22 20:21:50 -0600

    Add files via upload

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'capption',
+    appId: '1:903028655337:web:ab313c59bb1147ab491c78',
+    databaseURL: 'https://capption-default-rtdb.firebaseio.com',
+    storageBucket: 'capption.appspot.com',
+    apiKey: 'AIzaSyBdRrPsQSoDzN9zYyu7_zWkDbHqjavYPgs',
+    authDomain: 'capption-42c16.firebaseapp.com',
+    messagingSenderId: '903028655337',
+  },
   production: true
 };
